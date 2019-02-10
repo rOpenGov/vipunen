797173639c20897f8611fbf41a38d4d6ff12f721
Joona Lehtomäki <joona.lehtomaki@iki.fi>
2019-02-10 16:11:00 +0200

    Make it possible to drop cache in tests

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,1 +1,7 @@
 library(httptest)
+
+if (Sys.getenv("MOCK_BYPASS") == "true") {
+  with_mock_api <- force
+} else if (Sys.getenv("MOCK_BYPASS") == "capture") {
+  with_mock_api <- capture_requests
+}
