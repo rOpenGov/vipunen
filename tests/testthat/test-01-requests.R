1944208bcc92a262e9cbde57a6bc17fba8532ef4
Joona Lehtomäki <joona.lehtomaki@iki.fi>
2019-02-10 19:19:34 +0200

    Add tests for get_api_parameters()

--- tests/testthat/test-01-requests.R
+++ tests/testthat/test-01-requests.R
@@ -43,3 +43,27 @@ httptest::without_internet({
                  "GET http://api.vipunen.fi/api/resources")
   })
 })
+
+testthat::context("Publications")
+
+httptest::with_mock_api({
+
+  api_url <- "api/resources/julkaisut"
+
+  test_that("Requests happen", {
+    expect_is(httpcache::GET(RESOURCES_URL), "response")
+  })
+
+  test_that("Vipunen API is correctly parsed", {
+    expect_is(vipunen_api(api_url), "vipunen_api")
+    expect_identical(vipunen_api(api_url)$path,
+                     api_url)
+  })
+
+  test_that("High-level functions work for publications", {
+    resp <- get_api_parameters("api/resources/julkaisut")
+    expect_equal(ncol(resp), 2)
+    expect_identical(names(resp), c("name", "type"))
+  })
+
+})
