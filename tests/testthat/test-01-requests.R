ac43b747e12c38c61ee92320c377408c38e6bd21
Joona Lehtomäki <joona.lehtomaki@iki.fi>
2019-02-10 16:29:26 +0200

    Basic tests

---
+++ tests/testthat/test-01-requests.R
@@ -0,0 +1,40 @@
+RESOURCES <- list("avoin_yliopisto", "erilliset_opinto_oikeudet", "suoritteet",
+                  "toimipisteet", "avoin_amk", "amk_talous", "koulutusluokitus",
+                  "yo_talous", "suorittanut55", "ytl_arvosanat",
+                  "harjoittelukoulut", "alayksikkokoodisto",
+                  "koulutuksenkustannukset", "toimitilat", "henkilosto",
+                  "muu_henkilosto_amk", "taydennyskoulutukset", "opinnaytetyot",
+                  "julkaisut")
+
+RESOURCES_URL <- "http://api.vipunen.fi/api/resources"
+
+testthat::context("Requests")
+
+httptest::with_mock_api({
+
+  api_url <- "api/resources"
+
+  test_that("Requests happen", {
+    expect_is(httpcache::GET(RESOURCES_URL), "response")
+  })
+  test_that("Vipunen API is correctly parsed", {
+    expect_is(vipunen_api(api_url), "vipunen_api")
+    expect_identical(vipunen_api(api_url)$content,
+                     RESOURCES)
+    expect_identical(vipunen_api(api_url)$path,
+                     api_url)
+  })
+})
+
+testthat::context("Cache")
+
+httptest::without_internet({
+  test_that("When the cache is set, can read from it even with no connection", {
+    expect_identical(httpcache::GET("http://api.vipunen.fi/api/resources")$url,
+                     "http://api.vipunen.fi/api/resources")
+  })
+  test_that("But uncached() prevents reading from the cache", {
+    expect_error(httpcache::uncached(httpcache::GET("http://api.vipunen.fi/api/resources")),
+                 "GET http://api.vipunen.fi/api/resources")
+  })
+})
