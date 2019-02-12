8a692297cbfa964f366dc16e6049dbcd272e3bd1
Joona Lehtomäki <joona.lehtomaki@iki.fi>
2019-02-12 20:03:10 +0200

    Split contexts to separate files

--- tests/testthat/test-01-requests.R
+++ tests/testthat/test-01-requests.R
@@ -1,27 +1,24 @@
-RESOURCES <- list("avoin_yliopisto", "erilliset_opinto_oikeudet", "suoritteet",
-                  "toimipisteet", "avoin_amk", "amk_talous", "koulutusluokitus",
-                  "yo_talous", "suorittanut55", "ytl_arvosanat",
-                  "harjoittelukoulut", "alayksikkokoodisto",
-                  "koulutuksenkustannukset", "toimitilat", "henkilosto",
-                  "muu_henkilosto_amk", "taydennyskoulutukset", "opinnaytetyot",
-                  "julkaisut")
-
-RESOURCES_URL <- "http://api.vipunen.fi/api/resources"
-
-testthat::context("Generic requests")
+testthat::context("Requests")
 
 httptest::with_mock_api({
-
+  resources_url <- "http://api.vipunen.fi/api/resources"
   api_url <- "api/resources"
+  resources <- list("avoin_yliopisto", "erilliset_opinto_oikeudet", "suoritteet",
+                    "toimipisteet", "avoin_amk", "amk_talous", "koulutusluokitus",
+                    "yo_talous", "suorittanut55", "ytl_arvosanat",
+                    "harjoittelukoulut", "alayksikkokoodisto",
+                    "koulutuksenkustannukset", "toimitilat", "henkilosto",
+                    "muu_henkilosto_amk", "taydennyskoulutukset", "opinnaytetyot",
+                    "julkaisut")
 
   test_that("Requests happen", {
-    expect_is(httpcache::GET(RESOURCES_URL), "response")
+    expect_is(httpcache::GET(resources_url), "response")
   })
 
   test_that("Vipunen API is correctly parsed", {
     expect_is(vipunen_api(api_url), "vipunen_api")
     expect_identical(vipunen_api(api_url)$content,
-                     RESOURCES)
+                     resources)
     expect_identical(vipunen_api(api_url)$path,
                      api_url)
   })
@@ -29,41 +26,4 @@ httptest::with_mock_api({
   test_that("Non-existing path causes a well-behaving error", {
     expect_error(vipunen_api("foo/bar"))
   })
-})
-
-testthat::context("Cache")
-
-httptest::without_internet({
-  test_that("When the cache is set, can read from it even with no connection", {
-    expect_identical(httpcache::GET("http://api.vipunen.fi/api/resources")$url,
-                     "http://api.vipunen.fi/api/resources")
-  })
-  test_that("But uncached() prevents reading from the cache", {
-    expect_error(httpcache::uncached(httpcache::GET("http://api.vipunen.fi/api/resources")),
-                 "GET http://api.vipunen.fi/api/resources")
-  })
-})
-
-testthat::context("Publications")
-
-httptest::with_mock_api({
-
-  api_url <- "api/resources/julkaisut"
-
-  test_that("Requests happen", {
-    expect_is(httpcache::GET(RESOURCES_URL), "response")
-  })
-
-  test_that("Vipunen API is correctly parsed", {
-    expect_is(vipunen_api(api_url), "vipunen_api")
-    expect_identical(vipunen_api(api_url)$path,
-                     api_url)
-  })
-
-  test_that("High-level functions work for publications", {
-    resp <- get_api_parameters("api/resources/julkaisut")
-    expect_equal(ncol(resp), 2)
-    expect_identical(names(resp), c("name", "type"))
-  })
-
 })
---
+++ tests/testthat/test-03-publications.R
@@ -0,0 +1,24 @@
+testthat::context("Publications")
+
+httptest::with_mock_api({
+
+  resources_url <- "http://api.vipunen.fi/api/resources"
+  api_url <- "api/resources/julkaisut"
+
+  test_that("Requests happen", {
+    expect_is(httpcache::GET(resources_url), "response")
+  })
+
+  test_that("Vipunen API is correctly parsed", {
+    expect_is(vipunen_api(api_url), "vipunen_api")
+    expect_identical(vipunen_api(api_url)$path,
+                     api_url)
+  })
+
+  test_that("High-level functions work for publications", {
+    resp <- get_api_parameters("api/resources/julkaisut")
+    expect_equal(ncol(resp), 2)
+    expect_identical(names(resp), c("name", "type"))
+  })
+
+})
