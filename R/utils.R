557f8b59420d206ccfc905baf863e3508998241a
Joona Lehtomäki <joona.lehtomaki@iki.fi>
2019-02-12 21:27:10 +0200

    Implement valid_resource()

---
+++ R/utils.R
@@ -0,0 +1,20 @@
+#' valid_resource
+#'
+#' Test if a provided argument is a valid resource in Vipunen API. Valid
+#' resources are fetched from the API.
+#'
+#' @param x character name of the resource
+#'
+#' @return logical TRUE/FALSE
+#' @export
+#'
+#' @examples
+#'   # TRUE
+#'   valid_resource("julkaisut")
+#'   # FALSE
+#'   valid_resource("foobar")
+#'
+valid_resource <- function(x) {
+  resources <- unlist(vipunen_api("api/resources")$content)
+  return(x %in% resources)
+}
