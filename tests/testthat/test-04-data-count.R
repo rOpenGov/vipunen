f6d29dfbfe0d6be5c6b6a6acd23b2d1e8618d943
Joona Lehtomäki <joona.lehtomaki@iki.fi>
2019-02-16 18:32:13 +0200

    Test incorrect resource name

--- tests/testthat/test-04-data-count.R
+++ tests/testthat/test-04-data-count.R
@@ -2,6 +2,11 @@ testthat::context("Data counts")
 
 httptest::with_mock_api({
 
+  test_that("Only correct resource names are accepted", {
+    # This should raise an error
+    expect_error(get_data_count("foobar"))
+  })
+
   test_that("Correct counts are returned", {
     # resource = "avoin_yliopisto"
     expect_equal(get_data_count("avoin_yliopisto"), 342)
